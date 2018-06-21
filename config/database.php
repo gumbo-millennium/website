3d8668b2641f77a5bdf594d0a5433c70b8ea79a2
Roelof Roos <github@roelof.io>
2018-06-21 14:10:31 +0200

    Added Corcel

--- config/database.php
+++ config/database.php
@@ -78,6 +78,21 @@ return [
             'prefix' => '',
         ],
 
+        'wordpress' => [
+            'driver' => 'mysql',
+            'host' => env('WP_HOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('WP_PORT', env('DB_PORT', '3306')),
+            'database' => env('WP_DATABASE', 'forge'),
+            'username' => env('WP_USERNAME', 'forge'),
+            'password' => env('WP_PASSWORD', ''),
+            'unix_socket' => env('WP_SOCKET', env('DB_SOCKET', '')),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => 'wp_',
+            'strict' => true,
+            'engine' => null,
+        ],
+
     ],
 
     /*
