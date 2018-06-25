049ad53aabadfb856807ce5d04df45a954cdaff9
Roelof Roos <github@roelof.io>
2018-06-26 01:07:25 +0200

    Updated docker configs
    Databases now use UTF-8 by default, instead of latin1.

--- library/docker/mariadb/00-init.sql
+++ library/docker/mariadb/00-init.sql
@@ -1,8 +1,19 @@
 -- Create databases
-CREATE SCHEMA IF NOT EXISTS laravel;
-CREATE SCHEMA IF NOT EXISTS laravel_test;
-CREATE SCHEMA IF NOT EXISTS wordpress;
-CREATE SCHEMA IF NOT EXISTS wordpress_test;
+CREATE SCHEMA IF NOT EXISTS laravel
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_unicode_ci;
+
+CREATE SCHEMA IF NOT EXISTS laravel_test
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_unicode_ci;
+
+CREATE SCHEMA IF NOT EXISTS wordpress
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_unicode_ci;
+
+CREATE SCHEMA IF NOT EXISTS wordpress_test
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_unicode_ci;
 
 -- Create users
 CREATE USER IF NOT EXISTS laravel
@@ -11,8 +22,10 @@ CREATE USER IF NOT EXISTS corcel
     IDENTIFIED BY 'zie2doboveesh2IraiDaim1Daiku6ue9';
 CREATE USER IF NOT EXISTS wordpress
     IDENTIFIED BY 'aibaiwohPaighaikaevaoThiochahX5v';
+CREATE USER IF NOT EXISTS phpmyadmin
+    IDENTIFIED BY 'etah2raith4Thee5eijeiYei(f3foh8I';
 
--- Grant access to tables
+-- Grant Laravel all access to their own databases
 GRANT ALL
     ON laravel.*
     TO laravel;
@@ -21,11 +34,20 @@ GRANT ALL
     ON laravel_test.*
     TO laravel;
 
+-- Grant WordPress all access to their own database
 GRANT ALL
     ON wordpress.*
     TO wordpress;
 
+-- Restrict Corcel WordPress user to data-only access
 GRANT SELECT, INSERT, UPDATE, DELETE
     ON wordpress.*
     TO corcel;
 
+-- Restrict phpMyAdmin user to data-only access
+GRANT SELECT, INSERT, UPDATE, DELETE
+    ON wordpress.*
+    TO phpmyadmin;
+GRANT SELECT, INSERT, UPDATE, DELETE
+    ON laravel.*
+    TO phpmyadmin;
