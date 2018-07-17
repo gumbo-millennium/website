4cf97cf5eea8b87fff88d659bc2b4c3e23639d81
Roelof Roos <github@roelof.io>
2018-07-17 18:56:02 +0200

    Added gitignore update command and Guzzle

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Services\MenuProvider;
+use GuzzleHttp\Client;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,6 +29,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Register Guzzle client (singleton)
+        $this->app->singleton(Client::class, function () {
+
+            // Publish our name, some version info and how to contact us.
+            $userAgent = sprintf(
+                'gumbo-millennium.nl/1.0 (incompatible; curl/%s; php/%s; https://www.gumbo-millennium.nl);',
+                curl_version()['version'],
+                PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION
+            );
+
+            // The client should send a user agent that allows sysadmins to contact us,
+            // aside from that we should be snappy with declining the connection and not
+            // throw exceptions on response codes ≥ 400.
+            return new Client([
+                'http_errors' => false,
+                'connect_timeout' => 0.50,
+                'headers' => ["User-Agent: {$userAgent}"]
+            ]);
+        });
     }
 }
