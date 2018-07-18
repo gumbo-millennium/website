b4f4917e6ad5c0f8568c1a6b511e8494b1d4e363
Roelof Roos <github@roelof.io>
2018-07-18 16:01:17 +0200

    Footer legal menu

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,86 +1,89 @@
+@inject('menuHelper', 'App\Services\MenuProvider')
+
 {{-- Footer --}}
 <footer class="footer" id="footer">
     <div class="container">
-      <div class="row">
-        <div class="col-md-4">
-          <div class="title">
-            Over Gumbo Millennium
-          </div>
-          <ul class="menu">
-            <li>
-              <a href="#">Home Pages</a>
-            </li>
-            <li>
-              <a href="#">Theme Features</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">StoreFront</a>
-            </li>
-            <li>
-              <a href="#">Portfolio</a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-4">
-          <div class="title">
-            Connect with us
-          </div>
-          <ul class="menu">
-            <li>
-              <a href="#">
-                <i class="fa fa-instagram"></i>
-                Instagram
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i class="fa fa-twitter"></i>
-                Twitter
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i class="fa fa-dribbble"></i>
-                Dribbble
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i class="fa fa-facebook"></i>
-                Facebook
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-4 newsletter">
-          <div class="title">
-            Get awesome updates
-          </div>
-          <p>
-            Enter your email address for news and product launches in the Awesome Space.
-          </p>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="title">
+                    Over Gumbo Millennium
+                </div>
+                <ul class="menu">
+                    <li>
+                        <a href="#">Home Pages</a>
+                    </li>
+                    <li>
+                        <a href="#">Theme Features</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">StoreFront</a>
+                    </li>
+                    <li>
+                        <a href="#">Portfolio</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <div class="title">
+                    Connect with us
+                </div>
+                <ul class="menu">
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-instagram"></i>
+                            Instagram
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-twitter"></i>
+                            Twitter
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-dribbble"></i>
+                            Dribbble
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-facebook"></i>
+                            Facebook
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4 newsletter">
+                <div class="title">
+                    Get awesome updates
+                </div>
+                <p>
+                    Enter your email address for news and product launches in the Awesome Space.
+                </p>
 
-          <form class="newsletter-form" autocomplete="off">
-            <input type="email" id="mc-email" class="form-control" placeholder="Email address" required />
+                <form class="newsletter-form" autocomplete="off">
+                    <input type="email" id="mc-email" class="form-control" placeholder="Email address" required />
 
-            <button type="submit">
-              <i class="fa fa-chevron-right"></i>
-            </button>
-            <label for="mc-email" class="text-white newsletter-feedback mt-3"></label>
-          </form>
+                    <button type="submit">
+                        <i class="fa fa-chevron-right"></i>
+                    </button>
+                    <label for="mc-email" class="text-white newsletter-feedback mt-3"></label>
+                </form>
+            </div>
         </div>
-      </div>
-      <div class="bottom">
-        <ul>
-          <li><a href="#">Privacy</a></li>
-          <li><a href="#">Legal</a></li>
-          <li><a href="#">Language</a></li>
-        </ul>
-      </div>
+        @if ($menuHelper->hasLocation('footer-legal'))
+        <div class="bottom">
+            <ul>
+                @foreach ($menuHelper->location('footer-legal') as $menuItem)
+                <li><a href="{{ $menuItem['url'] }}">{{ $menuItem['title'] }}</a></li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
     </div>
-  </footer>
+</footer>
 <!-- Footer end-->
-<div class="text-center"><span class="copyright">© 1991 – {{ date('Y', strtotime('+14 days')) }} Gumbo Millennium</span></div>
