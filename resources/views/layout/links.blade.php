4575fb4afe2ce012dea569d216e122ad2fde4d67
Roelof Roos <github@roelof.io>
2018-07-19 17:46:23 +0200

    Fixed theme variables
    Some were missing, some are new. I removed a fuckload of unsupported fonts.
    
    (stuped Mac designers not cheking Linux and Windows)

--- resources/views/layout/links.blade.php
+++ resources/views/layout/links.blade.php
@@ -8,18 +8,13 @@
 <link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-touch-icon-114x114.png">
 
 {{-- Main stylesheet --}}
-<!-- Website theme -->
 <link href="{{ mix('/gumbo.css') }}" rel="stylesheet">
 
 {{-- Web fonts --}}
-<link href="https://fonts.googleapis.com/css?family=Hind:400,700%7cLora:400i%7cPoppins:500,600,700" rel="stylesheet">
-
-{{-- Font Awesome (pro or free) --}}
-@if (config('app.use-font-awesome-pro'))
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.0.13/css/regular.css" integrity="sha384-HLkkol/uuRVQDnHaAwidOxb1uCbd78FoGV/teF8vONYKRP9oPQcBZKFdi3LYDy/C" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.0.13/css/light.css" integrity="sha384-d8NbeymhHpk+ydwT2rk4GxrRuC9pDL/3A6EIedSEYb+LE+KQ5QKgIWTjYwHj/NBs" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.0.13/css/fontawesome.css" integrity="sha384-LDuQaX4rOgqi4rbWCyWj3XVBlgDzuxGy/E6vWN6U7c25/eSJIwyKhy9WgZCHQWXz" crossorigin="anonymous">
+@if (optional($page_type) === 'blog')
+    {{-- Blog pages have classier fonts, and Open Sans --}}
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700|Open+Sans:400,700|Zilla+Slab" rel="stylesheet">
 @else
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/regular.css" integrity="sha384-EWu6DiBz01XlR6XGsVuabDMbDN6RT8cwNoY+3tIH+6pUCfaNldJYJQfQlbEIWLyA" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/fontawesome.css" integrity="sha384-GVa9GOgVQgOk+TNYXu7S/InPTfSDTtBalSgkgqQ7sCik56N9ztlkoTr2f/T44oKV" crossorigin="anonymous">
+    {{-- Other pages just have the normal fonts --}}
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans:400,700" rel="stylesheet">
 @endif
