e68064e992dfafb010de55a568555828b9cfb0b6
Roelof Roos <github@roelof.io>
2018-11-02 20:06:36 +0100

    Fix for permissions command

--- app/Console/Commands/ListRolePermissions.php
+++ app/Console/Commands/ListRolePermissions.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
+use Symfony\Component\Console\Helper\TableCell;
 
 class ListRolePermissions extends Command
 {
@@ -60,15 +61,26 @@ class ListRolePermissions extends Command
             $scopedList->push(
                 collect($perm->$view)
                     ->concat($allRoles->map(function ($role) use ($perm) {
-                        return $perm->roles->contains($role) ? '<info>✓</>' : '';
+                        return $perm->roles->contains($role) ? '<info>✔</>' : '';
                     }))
             );
         }
 
+        // Generate headers
         $headers = collect(['Permission'])
             ->concat($allRoles->map(function ($role) use ($view) {
                 return $role->$view;
             }));
+
+        // Check if scopedList is empty
+        if ($scopedList->count() == 0) {
+            $scopedList->push([
+                new TableCell(
+                    '<comment>No results found.</>',
+                    ['colspan' => $headers->count()]
+                )
+            ]);
+        }
 
         $this->table($headers, $scopedList);
     }
