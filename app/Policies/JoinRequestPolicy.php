3d88dc65d0e46a62c950be1421df7ee3696bc03b
Roelof Roos <github@roelof.io>
2018-11-15 02:47:04 +0100

    Use JoinRequestPolicy on Requests\JoinChangeRequest

--- app/Policies/JoinRequestPolicy.php
+++ app/Policies/JoinRequestPolicy.php
@@ -34,6 +34,22 @@ class JoinRequestPolicy
     }
 
     /**
+     * Checks if the user is allowed to manage join requests
+     *
+     * @param User $user
+     * @return void
+     */
+    public function manage(User $user)
+    {
+        return $user->hasAnyPermission([
+            'join.update',
+            'join.accept',
+            'join.decline',
+            'join.delete'
+        ]);
+    }
+
+    /**
      * Determine whether the user can update the join request.
      *
      * @param  \App\User  $user
@@ -42,7 +58,47 @@ class JoinRequestPolicy
      */
     public function update(User $user, JoinRequest $joinRequest)
     {
-        return ($user->is($joinRequest->owner) || $user->hasPermissionTo('join-request-manage'));
+        return ($user->is($joinRequest->owner) || $user->hasPermissionTo('join.update'));
+    }
+
+    /**
+     * Returns if the user is allowed to accept the given join request
+     *
+     * @param User $user
+     * @param JoinRequest $joinRequest
+     * @return bool
+     */
+    public function accept(User $user, JoinRequest $joinRequest) : bool
+    {
+        $perms = ['join.accept', 'join.update'];
+
+        // You need the decline permission to un-decline a request
+        if ($joinRequest->is_declined) {
+            $perms[] = 'join.decline';
+        }
+
+        // Return if all permissions are granted
+        return $user->hasAllPermissions($perms);
+    }
+
+    /**
+     * Returns if the user is allowed to decline the given join request
+     *
+     * @param User $user
+     * @param JoinRequest $joinRequest
+     * @return bool
+     */
+    public function decline(User $user, JoinRequest $joinRequest) : bool
+    {
+        $perms = ['join.decline', 'join.update'];
+
+        // You need the accept permission to un-accept a request
+        if ($joinRequest->is_accepted) {
+            $perms[] = 'join.accept';
+        }
+
+        // Return if all permissions are granted
+        return $user->hasAllPermissions($perms);
     }
 
     /**
@@ -54,6 +110,6 @@ class JoinRequestPolicy
      */
     public function delete(User $user, JoinRequest $joinRequest)
     {
-        return ($user->is(joinRequest->owner)) || $user->hasPermissionTo('join-request-manage');
+        return $user->is($joinRequest->owner) || $user->hasPermissionTo('join.delete');
     }
 }
