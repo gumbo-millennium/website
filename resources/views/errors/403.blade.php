8e8b7c633aa8596d18a3ef1fadd33318a5443221
Roelof Roos <github@roelof.io>
2018-12-13 16:14:25 +0100

    Added 403, 404 and 500 error messages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.default')
+
+@section('content')
+<div
+    class="hero hero--large hero--with-cover"
+    >
+    <div class="hero__container">
+        <h3 class="hero__header">Geen toegang</h3>
+        <p class="hero__lead">Je bent niet gemachtigd om deze pagina te bekijken.</p>
+        <div class="hero__cta">
+            <a class="btn btn-brand hero__cta-btn" href="/">Naar homepage</a>
+        </div>
+    </div>
+</div>
+@endsection
