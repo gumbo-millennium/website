0cd15b075ebb23e0d4fc007d2d03ebc555ed8b7a
Roelof Roos <github@roelof.io>
2018-12-13 16:14:25 +0100

    Added 403, 404 and 500 error messages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.default')
+
+@section('content')
+<div
+    class="hero hero--large hero--with-cover"
+    >
+    <div class="hero__container">
+        <h3 class="hero__header">Pagina niet gevonden</h3>
+        <p class="hero__lead">Sorry! De aangevraagde pagina konden we niet vinden.</p>
+        <div class="hero__cta">
+            <a class="btn btn-brand hero__cta-btn" href="/">Naar homepage</a>
+        </div>
+    </div>
+</div>
+@endsection
