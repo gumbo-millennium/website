4cb44eb4304141c3a88a9935161140dbf84a2915
Roelof Roos <github@roelof.io>
2019-06-06 14:25:53 +0200

    Added genders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,11 +2,22 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Models\User::class, function (Faker $faker) {
+$randomGenders = [
+    'Man',
+    'Vrouw',
+    'Onbekend',
+    'Apache Gevechtshelikopter',
+    'Aggressieve wasbeer',
+];
+
+$factory->define(App\User::class, function (Faker $faker) use (
+    $randomGenders
+) {
     return [
         'first_name' => $faker->firstName,
         'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
+        'gender' => $faker->randomElement($randomGenders),
         'password' => password_hash('secret', PASSWORD_DEFAULT), // secret
         'remember_token' => str_random(10),
     ];
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,22 +13,37 @@ use Illuminate\Support\Facades\Hash;
 class UserSeeder extends Seeder
 {
     /**
+     * Creates a user
+     *
+     * @param string $email
+     * @param array $roles
+     * @return void
+     */
+    private function makeUser(string $email, string $name, array $roles) : void
+    {
+        $user = User::withTrashed()->updateOrCreate([
+            'email' => $email
+        ], [
+            'first_name' => $name,
+            'last_name' => 'Gumbo (test)',
+            'password' => Hash::make('Gumbo')
+        ]);
+        $user->assignRole($roles);
+
+        if ($user->trashed()) {
+            $user->restore();
+        }
+    }
+    /**
      * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $user = User::withTrashed()->updateOrCreate([
-            'email' => 'gumbo@docker.local'
-        ], [
-            'last_name' => 'Gumbo Millennium',
-            'password' => Hash::make('Gumbo')
-        ]);
-        $user->assignRole(['guest', 'member', 'dc']);
-
-        if ($user->trashed()) {
-            $user->restore();
-        }
+        $this->makeUser('board@example.com', 'Bestuur', ['guest', 'member', 'board']);
+        $this->makeUser('dc@example.com', 'DC', ['guest', 'member', 'dc']);
+        $this->makeUser('gumbo@example.com', 'Lid', ['guest', 'member']);
+        $this->makeUser('guest@example.com', 'Gast', ['guest']);
     }
 }
