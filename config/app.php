3111084e18fa04f6a53b3af1ac1b252f29803954
Roelof Roos <github@roelof.io>
2019-06-19 18:09:26 +0200

    Added Laravel Nova

--- config/app.php
+++ config/app.php
@@ -155,6 +155,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\NovaServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
