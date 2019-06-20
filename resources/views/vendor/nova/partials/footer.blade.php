d3edcbaef329230f722a3a5573e3a8296aea842d
Roelof Roos <github@roelof.io>
2019-06-20 17:06:43 +0200

    Updated navbar and footer

---
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<p class="mt-8 mb-4 text-center text-xs text-80">
+    © {{ now()->year }} Gumbo Millennium - Alle rechten voorbehouden
+    </p>
+
+<p class="text-center text-xs text-80">
+    <a href="https://gumbo-millennium.nl/" class="text-primary dim no-underline">Homepage</a>
+    <span class="px-1">&middot;</span>
+    <a href="https://gumbo-millennium.nl/privacy-policy" class="text-primary dim no-underline">Privacy Policy</a>
+    <span class="px-1">&middot;</span>
+    Admin Panel v{{ Laravel\Nova\Nova::version() }}
+</p>
