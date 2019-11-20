f714375b9c887eabf69b03229c7db9e5d1f62af5
Roelof Roos <github@roelof.io>
2019-11-20 11:51:02 +0100

    Re-introduced e-mail field on login form
    Somehow, it got removed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,6 +16,19 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
