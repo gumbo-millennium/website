1ae542446e15ed5ebc15968ba07fc1ddf89a55a6
Roelof Roos <github@roelof.io>
2019-12-16 05:14:30 +0100

    Fixed quick login form on local env

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,40 @@
-@extends('layout.main')
+@extends('layout.variants.login')
+
+@php
+$testUsers = app()->isLocal() ? App\Models\User::where('email', 'LIKE', '%@example.gumbo-millennium.nl')->get() : [];
+@endphp
 
 @section('login-content')
 {{-- Header --}}
 <h1 class="login__header text-4xl">{{ __('Login') }}</h1>
+
+{{-- Auto login form --}}
+@if ($testUsers)
+<div class="my-8 bg-white rounded shadow p-8">
+    <form method="POST" action="{{ route('login') }}" class="login__form">
+        @csrf
+        <input type="hidden" name="password" value="Gumbo" />
+
+        <p class="mb-4">Testing mode enabled. Pick a user to log in as.</p>
+
+        {{-- Login user --}}
+        <div class="mb-4 login__field">
+            {{-- Label --}}
+            <label for="user" class="login__field-label block text-sm mb-2">User</label>
+
+            {{-- Field --}}
+            <select name="email" id="user" class="login__field-input form-select block">
+                @foreach ($testUsers as $user)
+                <option value="{{ $user->email }}">{{ $user->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        {{-- Submit button --}}
+        <button class="login__submit block btn btn-brand mb-4" type="submit">Inloggen</button>
+    </form>
+</div>
+@endif
 
 {{-- Form --}}
 <form method="POST" action="{{ route('login') }}" class="login__form">
