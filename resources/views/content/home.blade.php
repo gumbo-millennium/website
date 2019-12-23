deacb8962661b318abf26c7395ca5a65ffddb3a2
Roelof Roos <github@roelof.io>
2019-12-23 19:38:10 +0100

    Homepage activity

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -13,24 +13,8 @@
 
 {{-- Upcoming --}}
 @if ($nextEvent)
-<div class="bg-brand-100 activity-block after-header">
-    <div class="container activity-block__container">
-        <div class="row">
-            <div class="col activity-block__date-col">
-                <time datetime="{{ $nextEvent->start_date->toIso8601String() }}" class="activity-block__date">
-                    <div class="activity-block__date-day">
-                        {{ $nextEvent->start_date->day }}
-                    </div>
-                    <div class="activity-block__date-month">
-                        {{ $nextEvent->start_date->isoFormat('MMM') }}
-                    </div>
-                </time>
-            </div>
-            <div class="col p-4">
-                <h1>I am the content</h1>
-            </div>
-        </div>
-    </div>
+<div class="bg-brand-50 after-header">
+    @include('activities.bits.list-item', ['activity' => $nextEvent])
 </div>
 @endif
 @endsection
