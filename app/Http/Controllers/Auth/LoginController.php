9982656138ba560f745b22df7eb827dd04a94192
Roelof Roos <github@roelof.io>
2020-01-21 02:18:04 +0100

    Use the `Clear-Site-Data` HTTP header on logout
    Clears cookies and cache when you logout. Prevents the back-button from
    doing something useful after you switch accounts.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -18,7 +19,9 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers {
+        logout as private doLogout;
+    }
     use RedirectsToAdminHomeTrait;
 
     /**
@@ -29,5 +32,18 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        return $this->doLogout($request)->withHeaders([
+            'Clear-Site-Data' => ['"cache"', '"cookies"']
+        ]);
     }
 }
