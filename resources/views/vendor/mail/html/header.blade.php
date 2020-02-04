2a53360e2965e46c3c29392029674ea3f9c67d41
Roelof Roos <github@roelof.io>
2020-02-04 18:32:12 +0100

    Small mail fixes

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,21 @@
 <tr>
-    <td class="mail-header">
-        <a href="{{ $url }}">
-            {{ $slot }}
-        </a>
+    <td>
+        <table class="header" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+            <tr>
+                <td class="content-cell content-cell--header" align="center">
+                    <a href="{{ $url }}" class="px-8 py-4">
+                        <img
+                            class="header__logo"
+                            width="192"
+                            height="64"
+                            src="{{ asset('email/logo.png') }}"
+                            srcset="{{ asset('email/logo.svg') }} 192w, {{ asset('email/logo@2x.png') }} 384w, {{ asset('email/logo@3x.png') }} 576w, {{ asset('email/logo@4x.png') }} 768w"
+                            alt="Gumbo Millennium"
+                            border="0"
+                            />
+                    </a>
+                </td>
+            </tr>
+        </table>
     </td>
 </tr>
