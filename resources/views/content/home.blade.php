819c0171591a6fa12ab1b37249d847afd8ee2ec8
Roelof Roos <github@roelof.io>
2020-02-14 09:04:59 +0100

    Changed the homepage

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -1,20 +1,30 @@
 @extends('layout.main')
 
 @section('content')
-{{-- Welcome --}}
-<div class="header header--clipped">
-    <div class="container header__container">
-        <h1 class="header__title">Welkom bij Gumbo Millennium</h1>
-        <p class="header__subtitle">
-            Dubbel L, dubbel N, <strong class="font-bold">dubbel genieten</strong>!
-        </p>
+<div class="container">
+    <div class="home-hero">
+        <div class="home-hero__text">
+            <h2 class="home-hero__title">
+                Dubbel L, Dubbel N,<br />
+                <span class="home-hero__title-fat">Dubbel genieten</span>
+            </h2>
+
+            <p class="home-hero__lead">Welkom bij de gezelligste studentenvereniging van Zwolle.</p>
+
+            <a href="{{ route('join.form') }}" class="btn btn--brand my-0">Word lid</a>
+        </div>
+
+        <div class="home-hero__logo">
+            <img src="{{ mix('images/logo-glass-green.svg') }}" alt="Gumbo Millennium" class="home-hero__logo-image" />
+        </div>
     </div>
 </div>
 
 {{-- Upcoming --}}
 @if (!empty($nextEvents))
 <div class="container pt-8">
-    <h2 class="text-3xl text-normal mb-8">Binnenkort bij Gumbo Millennium</h2>
+    <p class="text-center text-gray-600 mb-4">Altijd iets te doen</p>
+    <h2 class="text-3xl text-medium font-title mb-8 text-center">Binnenkort bij Gumbo Millennium</h2>
     {{-- Activity cards --}}
     <div class="activity-grid">
         @foreach ($nextEvents as $activity)
