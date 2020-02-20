5d83d8c93d9f761e14ca513e50f192476a5bdf30
Roelof Roos <github@roelof.io>
2020-02-20 17:58:17 +0100

    Fixed plazacam PUT-test

--- tests/Feature/UpdatePlazacamTest.php
+++ tests/Feature/UpdatePlazacamTest.php
@@ -19,13 +19,13 @@ class UpdatePlazacamTest extends TestCase
     use TempUserTrait;
 
     /**
-     * Test anonymous image retrieval
+     * Test assignment by guest
      * @return void
      */
-    public function testReadUser(): void
+    public function testWriteUser(): void
     {
         // Gets the user
-        $user = $this->getUser(['guest', 'member']);
+        $user = $this->getUser(['guest']);
 
         // Get the url
         $url = URL::signedRoute('api.plazacam.store', [
@@ -47,13 +47,69 @@ class UpdatePlazacamTest extends TestCase
     }
 
     /**
-     * Test anonymous image retrieval
+     * Test assignment by member
      * @return void
      */
-    public function testReadMember(): void
+    public function testWriteMember(): void
     {
         // Gets the all-knowning user
-        $user = $this->getUser(['guest', 'dc']);
+        $user = $this->getUser(['member']);
+
+        // Get the url
+        $url = URL::signedRoute('api.plazacam.store', [
+            'user' => $user->id,
+            'image' => 'plaza'
+        ]);
+
+        // Get a dummy file
+        $file = UploadedFile::fake()->image('capture.jpg', 800, 600);
+
+        //  Retrieve plazacam
+        $request = $this->actingAs($user)
+            ->json('PUT', $url, [
+                'file' => $file
+            ]);
+
+        // Expect an OK
+        $request->assertForbidden();
+    }
+
+    /**
+     * Test assignment by PC
+     * @return void
+     */
+    public function testWritePlazaCommittee(): void
+    {
+        // Gets the all-knowning user
+        $user = $this->getUser(['member', 'pc']);
+
+        // Get the url
+        $url = URL::signedRoute('api.plazacam.store', [
+            'user' => $user->id,
+            'image' => 'plaza'
+        ]);
+
+        // Get a dummy file
+        $file = UploadedFile::fake()->image('capture.jpg', 800, 600);
+
+        //  Retrieve plazacam
+        $request = $this->actingAs($user)
+            ->json('PUT', $url, [
+                'file' => $file
+            ]);
+
+        // Expect an OK
+        $request->assertSuccessful();
+    }
+
+    /**
+     * Test assignment by board
+     * @return void
+     */
+    public function testWriteBoard(): void
+    {
+        // Gets the all-knowning user
+        $user = $this->getUser(['member', 'board']);
 
         // Get the url
         $url = URL::signedRoute('api.plazacam.store', [
