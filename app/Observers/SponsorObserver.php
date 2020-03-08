6fefcfde931a12d01b9194cd9cc2226a7e110316
Roelof Roos <github@roelof.io>
2020-03-08 20:17:51 +0100

    Improve sponsor images

---
+++ app/Observers/SponsorObserver.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Observers;
+
+use App\Jobs\OptimizeUserSvg;
+use App\Models\Sponsor;
+
+class SponsorObserver
+{
+    /**
+     * Dispatches an SVG update if the logos were changed
+     * @param Sponsor $sponsor
+     * @return void
+     */
+    public function saved(Sponsor $sponsor): void
+    {
+        if ($sponsor->wasChanged('logo_gray')) {
+            OptimizeUserSvg::dispatch($sponsor->logo_gray, OptimizeUserSvg::TARGET_MONOTONE);
+        }
+
+        if ($sponsor->wasChanged('logo_color')) {
+            OptimizeUserSvg::dispatch($sponsor->logo_color, OptimizeUserSvg::TARGET_FULL_COLOR);
+        }
+    }
+}
