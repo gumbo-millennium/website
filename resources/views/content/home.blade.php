76508ad84c39fe0fb915ffd5e1c337954466f277
Roelof Roos <github@roelof.io>
2020-03-27 01:35:48 +0100

    Added covid message to homepage

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -32,6 +32,23 @@ SEOMeta::setCanonical(url('/'));
     </div>
 </div>
 
+<div class="bg-blue-100 text-lg font-normal">
+    <div class="container container--sm py-20 text-center">
+        {{-- Title --}}
+        <div class="flex flex-row justify-center items-center mb-4">
+            @icon('solid/virus', 'h-8 w-8 text-blue-600 mr-4')
+            <strong class="mb-2 md:mb-0 md:mr-4">Coronavirus informatie</strong>
+        </div>
+
+        <p class="mb-2">
+            De coronacrisis laat Gumbo Millennium ook niet ongeroerd. Ook wij hebben annuleringen, verplaatsingen
+            en regelwijzigingen doorgevoerd aan de hand van de Covid-19 uitbraak.
+        </p>
+
+        <a href="/coronavirus" class="text-lg text-blue-600 no-underline">Lees hier meer</a>
+    </div>
+</div>
+
 {{-- Upcoming --}}
 @if (!empty($nextEvents))
 <div class="container pt-8">
