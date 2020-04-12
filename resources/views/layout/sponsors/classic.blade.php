71778271f0cad3e06f1c26f3de69b60dc314aa8e
Roelof Roos <github@roelof.io>
2020-04-12 19:50:51 +0200

    Sponsor styling

--- resources/views/layout/sponsors/classic.blade.php
+++ resources/views/layout/sponsors/classic.blade.php
@@ -1,7 +1,10 @@
 <div class="sponsor">
     <div class="container sponsor__container">
         <a href="{{ route('sponsors.link', compact('sponsor')) }}" class="sponsor__simple-link">
-            <img src="{{ $sponsor->logo_color_url }}" alt="{{ $sponsor->title }}" class="sponsor__simple-logo">
+            <img
+                src="{{ $sponsor->logo_gray_url }}"
+                alt="{{ $sponsor->title }}"
+                class="sponsor__simple-logo fill-current">
         </a>
     </div>
 </div>
--- resources/views/layout/sponsors/modern.blade.php
+++ resources/views/layout/sponsors/modern.blade.php
@@ -1,17 +1,17 @@
 @php
-$sponsorClass = Str::slug("sponsor-block--{$sponsor->slug}");
+$sponsorClass = Str::slug("sponsor--brand-{$sponsor->slug}");
 @endphp
 @push('main.styles')
 <style nonce="{{ csp_nonce() }}">
-.{{ $sponsorClass }} {
+.sponsor--backdrop-brand {
     background-image: url("{{ $sponsor->backdrop->url('banner') }}");
 }
 </style>
 @endpush
-<div class="bg-white sponsor-block {{ $sponsorClass }}">
-    <div class="container my-8 text-center">
-        <a href="{{ route('sponsors.link', compact('sponsor')) }}">
-            <img src="{{ $sponsor->logo_color_url }}" alt="{{ $sponsor->title }}" class="w-64 h-20">
+<div class="sponsor sponsor--backdrop sponsor--backdrop-brand">
+    <div class="container sponsor__container">
+        <a href="{{ route('sponsors.link', compact('sponsor')) }}" class="sponsor__simple-link">
+            <img src="{{ $sponsor->logo_color_url }}" alt="{{ $sponsor->title }}" class="sponsor__simple-logo">
         </a>
     </div>
 </div>
