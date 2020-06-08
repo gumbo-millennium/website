4d901d8b382c93ebbbe79acc3e93875135981e5a
Roelof Roos <github@roelof.io>
2020-06-08 17:47:37 +0200

    Removed read time, since it's hardly used

--- app/Observers/NewsItemObserver.php
+++ app/Observers/NewsItemObserver.php
@@ -5,14 +5,9 @@ declare(strict_types=1);
 namespace App\Observers;
 
 use App\Models\NewsItem;
-use Carbon\CarbonInterface;
-use Html2Text\Html2Text;
-use Mtownsend\ReadTime\ReadTime;
 
 class NewsItemObserver
 {
-    private const WORDS_PER_MINUTE = 190;
-
     /**
      * Try to make sure a news article has an author
      * @param NewsItem $newsArticle
@@ -28,28 +23,5 @@ class NewsItemObserver
                 $newsArticle->author_id = $user->id;
             }
         }
-
-        // Determine text-only content
-        $content = (string) $newsArticle->html;
-        $textContent = (new Html2Text($content))->getText();
-
-        // Determine read time
-        $readTimeObject = new ReadTime($textContent, false, false, self::WORDS_PER_MINUTE);
-        $readTimeObject->abbreviated(true);
-        $readTimeObject->omitSeconds(false);
-        $readTimeData = $readTimeObject->toArray();
-        $readFinished = now()
-            ->addMinutes($readTimeData['minutes'])
-            ->addSeconds($readTimeData['seconds']);
-
-        $readTotalTime = $readFinished->diffInSeconds(now());
-        $readTime = $readFinished->diffForHumans([
-            'syntax' => CarbonInterface::DIFF_ABSOLUTE,
-            'options' => CarbonInterface::CEIL,
-            'parts' => 1
-        ]);
-
-        // Assign read time
-        $newsArticle->read_time = $readTotalTime ? $readTime : null;
     }
 }
