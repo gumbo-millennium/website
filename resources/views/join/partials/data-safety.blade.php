521d705696930b770df11d48f9f7ce1a9b330372
Tim van der Laan <46891201+Semprosx@users.noreply.github.com>
2020-06-30 18:44:04 +0200

    Update membership application text

--- resources/views/join/partials/data-safety.blade.php
+++ resources/views/join/partials/data-safety.blade.php
@@ -4,9 +4,10 @@
 </p>
 <p class="leading-relaxed">
     <small class="text-sm text-gray-primary-1">
-        Omdat jouw inschrijving niet betekend dat je lid wordt, slaan wij deze
-        gegevens versleuteld op.  Indien na 90 dagen nog steeds geen positief
-        besluit is genomen over je aanmelding, verwijderen wij je gegevens
-        automatisch. <strong>Hiervan krijg je bericht.</strong>
+        Omdat jouw inschrijving niet automatisch betekent dat je direct
+        lid bent, slaan wij deze gegevens versleuteld op. Indien na 90
+        dagen nog steeds geen positief besluit is genomen over je
+        aanmelding, verwijderen wij je gegevens automatisch.
+        <strong>Hiervan krijg je bericht.</strong>
     </small>
 </p>
