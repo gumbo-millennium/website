cc859ba3bb8ed76683a08e94ba9e3c2c2d924202
Roelof Roos <github@roelof.io>
2020-08-04 10:47:52 +0200

    Files - Automatic separation of date and name
    Also recognizes used separators and replaces them with a space, so
    browsers can do word wrapping more easily

--- resources/views/files/bits/file-item.blade.php
+++ resources/views/files/bits/file-item.blade.php
@@ -1,7 +1,23 @@
+@php
+// Get base values
+$fileDate = null;
+$fileName = $file->name;
+
+// Get matches
+if (preg_match('/^(\d{4}-\d{2}-\d{2})([^a-z0-9])(.+)$/i', $file->name, $matches)) {
+    $fileDate = $matches[1];
+    $separator = $matches[2];
+    $fileName = str_replace($separator, ' ', $matches[3]);
+}
+@endphp
+
 <div class="file-set__item">
     {{-- Get title --}}
     <a href="{{ route('files.download-single', ['media' => $file]) }}" class="file-set__item-title">
-        {{ $file->name }}
+        @if ($fileDate)
+        <span class="inline-block text-gray-primary-1 mr-1">{{ $fileDate }}</span>
+        @endif
+        {{ $fileName }}
     </a>
 
     {{-- Meta --}}
