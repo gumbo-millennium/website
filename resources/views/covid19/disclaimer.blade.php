0b3cbec62d13684c666d40915486ed7a15c31f2c
Roelof Roos <github@roelof.io>
2020-09-07 11:50:27 +0200

    Changed UI for Covid pages and notices

---
+++ resources/views/covid19/disclaimer.blade.php
@@ -0,0 +1,12 @@
+<div class="container">
+  <div class="notice notice--info notice--large">
+    <strong class="notice__title">Disclaimer</strong>
+    <p class="m-0 w-full">
+      De artikelen hier geven aan hoe Gumbo omgaat met de regels die door
+      de overheid worden gesteld, maar zijn hier geen vervanging van.<br />
+      Voor de meest actuele informatie, raden wij je altijd aan om de website
+      van de overheid in de gaten te houden.
+    </p>
+    @include('covid19.block', ['outbound' => 1])
+  </div>
+</div>
--- resources/views/covid19/link.blade.php
+++ resources/views/covid19/link.blade.php
@@ -1,5 +1,10 @@
-<a href="https://www.rijksoverheid.nl/onderwerpen/coronavirus-covid-19" rel="friend" target="__blank"
-    class="covid-btn">
-    @svg('solid/external-link-alt', 'mr-2')
-    <span>Meer informatie</span>
-</a>
+@if (!empty($outbound))
+    <a href="https://www.rijksoverheid.nl/onderwerpen/coronavirus-covid-19" rel="friend" target="__blank" class="covid-btn">
+      @svg('solid/external-link-alt', 'mr-2')
+      <span>Meer informatie</span>
+    </a>
+@else
+    <a href="{{ url('/coronavirus') }}" class="covid-btn">
+      <span>Meer informatie</span>
+    </a>
+@endif
