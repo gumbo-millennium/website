262841dc147631b31cf6f5bc8699a61189e01aa9
Roelof Roos <github@roelof.io>
2020-09-07 12:03:13 +0200

    Added Grote Clubactie sponsor block
    Closes #122

---
+++ resources/views/content/home/clubactie.blade.php
@@ -0,0 +1,17 @@
+<div class="bg-gray-secondary-2 font-normal">
+    <div class="container container--md py-8">
+      {{-- Title --}}
+      <p class="text-center text-gray-primary-1 mb-4">Grote Clubactie</p>
+      <h2 class="text-3xl text-medium font-title mb-8 text-center">Sponsor het lustrum en win prijzen</h2>
+
+      {{-- Lead text --}}
+      <p class="text-lg mb-2">In 2021 bestaat Gumbo <strong>30 jaar</strong>, en dat moet groots gevierd worden.</p>
+      <p>
+        Wil jij bijdragen aan het lustrum van Gumbo Millennium, en kans maken op leuke prijzen?<br />
+        Koop dan loten voor de Grote Clubactie!
+      </p>
+
+      {{-- Link --}}
+      <a href="https://clubactie.nl/lot/gumbo-millennium/10379399" target="_blank" rel="nofollow noopener" class="btn btn--brand">Koop nu je loten</a>
+    </div>
+</div>
