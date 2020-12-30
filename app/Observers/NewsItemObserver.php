7c2b48cacee8e2fa934d6cc71f9275b1553fba3c
Mark Walet <mark.walet@gmail.com>
2020-12-30 19:59:34 +0100

    Remove read_time attribute from source

--- app/Observers/NewsItemObserver.php
+++ app/Observers/NewsItemObserver.php
@@ -5,14 +5,9 @@ declare(strict_types=1);
 namespace App\Observers;
 
 use App\Models\NewsItem;
-use Carbon\CarbonInterface;
-use Html2Text\Html2Text;
-use Mtownsend\ReadTime\ReadTime;
 
 class NewsItemObserver
 {
-    private const WORDS_PER_MINUTE = 190;
-
     /**
      * Try to make sure a news article has an author
      *
@@ -22,35 +17,11 @@ class NewsItemObserver
     public function saving(NewsItem $newsArticle)
     {
         // Add user if missing
-        if ($newsArticle->author_id === null) {
-            $user = request()->user();
-            if ($user) {
-                // Update author if available
-                $newsArticle->author_id = $user->id;
-            }
+        if ($newsArticle->author_id !== null || auth()->check() === false) {
+            return;
         }
 
-        // Determine text-only content
-        $content = (string) $newsArticle->html;
-        $textContent = (new Html2Text($content))->getText();
-
-        // Determine read time
-        $readTimeObject = new ReadTime($textContent, false, false, self::WORDS_PER_MINUTE);
-        $readTimeObject->abbreviated(true);
-        $readTimeObject->omitSeconds(false);
-        $readTimeData = $readTimeObject->toArray();
-        $readFinished = now()
-            ->addMinutes($readTimeData['minutes'])
-            ->addSeconds($readTimeData['seconds']);
-
-        $readTotalTime = $readFinished->diffInSeconds(now());
-        $readTime = $readFinished->diffForHumans([
-            'syntax' => CarbonInterface::DIFF_ABSOLUTE,
-            'options' => CarbonInterface::CEIL,
-            'parts' => 1,
-        ]);
-
-        // Assign read time
-        $newsArticle->read_time = $readTotalTime ? $readTime : null;
+        // Update author if available
+        $newsArticle->author_id = auth()->user()->id;
     }
 }
