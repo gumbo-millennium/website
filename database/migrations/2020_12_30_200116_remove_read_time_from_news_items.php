84a5ee435a0a51b672ee536150b0bc8c426a8952
Mark Walet <mark.walet@gmail.com>
2020-12-30 20:02:45 +0100

    Add migration to remove the read_time property

---
+++ database/migrations/2020_12_30_200116_remove_read_time_from_news_items.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveReadTimeFromNewsItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('news_items', function (Blueprint $table) {
+            $table->dropColumn('read_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('news_items', function (Blueprint $table) {
+            $table->string('read_time', 15)->nullable()->default(null)->after('contents');
+        });
+    }
+}
