c1d5dcc5b622a138483ffac0e49c0ca42db3528d
Roelof Roos <github@roelof.io>
2021-01-02 02:38:17 +0100

    phpcs fixes

--- database/migrations/2020_12_30_200116_remove_read_time_from_news_items.php
+++ database/migrations/2020_12_30_200116_remove_read_time_from_news_items.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +15,7 @@ class RemoveReadTimeFromNewsItems extends Migration
      */
     public function up()
     {
-        Schema::table('news_items', function (Blueprint $table) {
+        Schema::table('news_items', static function (Blueprint $table) {
             $table->dropColumn('read_time');
         });
     }
@@ -25,7 +27,7 @@ class RemoveReadTimeFromNewsItems extends Migration
      */
     public function down()
     {
-        Schema::table('news_items', function (Blueprint $table) {
+        Schema::table('news_items', static function (Blueprint $table) {
             $table->string('read_time', 15)->nullable()->default(null)->after('contents');
         });
     }
