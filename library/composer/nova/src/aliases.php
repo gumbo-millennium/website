a311ce8014a80cc9207414bf090747a3245d513a
Roelof Roos <github@roelof.io>
2021-01-02 04:18:02 +0100

    Fixed Nova uses breaking the pipelines

---
+++ library/composer/nova/src/aliases.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Laravel\Nova;
+
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Actions\Action::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\ActionFields::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\Badge::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\BelongsTo::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\BelongsToMany::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\Boolean::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\DateTime::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\HasMany::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\Heading::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\ID::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\Image::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\KeyValue::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\Markdown::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\MorphToMany::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\Number::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\Select::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\Sparkline::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\Text::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\Textarea::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Fields\Trix::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Filters\BooleanFilter::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Filters\Filter::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Http\Requests\NovaRequest::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Metrics\Partition::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Metrics\Trend::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Metrics\Value::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\NovaApplicationServiceProvider::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Panel::class);
+class_alias(\Laravel\Nova\Nova::class, \Laravel\Nova\Resource::class);
