a948ecd1bda6296f2580fc63c660aeb9f370c74e
Roelof Roos <github@roelof.io>
2021-01-20 19:46:31 +0100

    Updated BotQuote factory to use states instead of randomness for sent state

--- database/seeds/BotQuoteSeeder.php
+++ database/seeds/BotQuoteSeeder.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use App\Models\BotQuote;
+use App\Models\User;
+use Faker\Generator;
 use Illuminate\Database\Seeder;
 
 class BotQuoteSeeder extends Seeder
@@ -12,8 +14,24 @@ class BotQuoteSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Generator $faker): void
     {
-        factory(BotQuote::class, 25)->create();
+        factory(BotQuote::class, 70)->state('sent')->create([
+            'user_id' => null,
+        ]);
+        factory(BotQuote::class, 15)->create([
+            'user_id' => null,
+        ]);
+
+        foreach (User::query()->where('email', 'like', '%@example.gumbo-millennium.nl')->get() as $user) {
+            factory(BotQuote::class)
+                ->times($faker->numberBetween(1, 30))
+                ->state('sent')
+                ->create(['user_id' => null]);
+
+            factory(BotQuote::class)
+                ->times($faker->numberBetween(1, 5))
+                ->create(['user_id' => null]);
+        }
     }
 }
