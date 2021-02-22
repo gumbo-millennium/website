b486ee62df219114fe2543d6b7fdb5d10c7d5718
Roelof Roos <github@roelof.io>
2021-02-22 21:20:25 +0100

    Added MemberReferral model and resource

---
+++ app/Models/MemberReferral.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * A referral by a user, managed by the Intro Committee
+ *
+ * @property string $subject
+ * @property string $referred_by
+ * @property int $user_id
+ * @property-read User|null $user
+ * @property int $id
+ * @property \Illuminate\Support\Date $created_at
+ * @property \Illuminate\Support\Date $updated_at
+ */
+class MemberReferral extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'subject',
+        'referred_by',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Nova/Resources/MemberReferral.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nova\Resources;
+
+use App\Models\MemberReferral as MemberReferralModels;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+
+// phpcs:disable SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
+
+class MemberReferral extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = MemberReferralModels::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+        'subject',
+        'referred_by',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+
+            Text::make(__('New member'), 'subject')
+                ->readonly(fn () => $this->exists),
+
+            Text::make(__('Referred by'), 'referred_by')
+                ->readonly(fn () => $this->exists),
+
+            BelongsTo::make(__('Referred by user'), 'user', User::class)
+                ->nullable()
+                ->searchable()
+                ->withoutTrashed(),
+        ];
+    }
+}
