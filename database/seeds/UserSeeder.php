c365eb14d44643912b225077b8030b47f40d9b68
Roelof Roos <github@roelof.io>
2021-03-25 23:09:36 +0100

    Fixed UserSeeder not including WC

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -27,6 +27,7 @@ class UserSeeder extends Seeder
         $this->makeUser('dc', 'DC', ['member', 'dc']);
         $this->makeUser('lhw', 'LHW', ['member', 'lhw']);
         $this->makeUser('ic', 'Intro Commissie', ['member', 'ic']);
+        $this->makeUser('wc', 'Werving Commissie', ['member', 'wc']);
         $this->makeUser('gumbo', 'Lid', ['member']);
         $this->makeUser('guest', 'Gast', ['guest']);
         $this->makeUser('event-owner', 'Event Owner', ['member']);
