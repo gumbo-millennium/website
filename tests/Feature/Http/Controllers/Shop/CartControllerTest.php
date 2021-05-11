9f93c0823951bd5869868844f41d553da8f36626
Roelof Roos <github@roelof.io>
2021-05-11 23:28:30 +0200

    Shop - Added CartController test

---
+++ tests/Feature/Http/Controllers/Shop/CartControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Http\Controllers\Shop;
+
+use App\Models\Shop\Category;
+use App\Models\Shop\Product;
+use App\Models\Shop\ProductVariant;
+use Darryldecode\Cart\Facades\CartFacade as Cart;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class CartControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testAddToCart(): void
+    {
+        $category = factory(Category::class)->create([
+            'visible' => true,
+        ]);
+
+        $product = factory(Product::class)->create([
+            'category_id' => $category->id,
+            'visible' => true,
+        ]);
+
+        $variant = factory(ProductVariant::class)->create([
+            'product_id' => $product->id,
+        ]);
+
+        $response = $this->onlyForMembers(route('shop.cart.add'), [
+            'variant' => $variant->id,
+            'quantity' => 1,
+        ], 'post');
+
+        $response->assertRedirect(route('shop.cart'));
+
+        $this->assertCount(1, Cart::getContent());
+    }
+}
