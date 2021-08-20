95610b794abc2e7683e559389df74625902a5854
Roelof Roos <github@roelof.io>
2021-08-20 17:02:28 +0200

    Added carbon-neutral footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,17 @@
-<footer class="footer">
-    <div class="container text-center">
-        <p>© Gumbo Millennium {{ today()->year }}. Alle rechten voorbehouden.</p>
+<footer class="footer container">
+    <div class="grid grid-cols-1 md:grid-cols-3">
+        <div class="hidden md:block">&nbsp;</div>
+
+        <div class="text-center">
+            <p>© Gumbo Millennium {{ today()->year }}. Alle rechten voorbehouden.</p>
+        </div>
+
+        <div class="hidden md:flex items-center justify-end text-gray-300">
+            @icon('solid/leaf', 'h-4 mr-2')
+
+            <a class="no-underline text-gray-300 hover:text-gray-700 hover:underline focus:text-gray-700 focus:underline" rel="nofollow noopener" target="_blank" href="https://www.scaleway.com/en/environmental-leadership/">
+                Onze website is CO<sub>2</sub> neutraal gehost.
+            </a>
+        </div>
     </div>
 </footer>
