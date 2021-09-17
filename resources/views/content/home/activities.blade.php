4077597f47c8cd50888e0bffcc66b27b80b2066b
Roelof Roos <github@roelof.io>
2021-09-17 18:20:06 +0200

    Padding fix

--- resources/views/content/home/activities.blade.php
+++ resources/views/content/home/activities.blade.php
@@ -1,6 +1,6 @@
 {{-- Upcoming --}}
 @if (!empty($nextEvents))
-<div class="container pt-8">
+<div class="container py-8">
     <p class="text-center text-gray-primary-1 mb-4">Altijd iets te doen</p>
     <h2 class="text-3xl text-medium font-title mb-8 text-center">Binnenkort bij Gumbo Millennium</h2>
     {{-- Activity cards --}}
