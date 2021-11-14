c8540ec287096f332317fb624541832b46d78c7f
Roelof Roos <github@roelof.io>
2021-11-14 17:42:14 +0100

    Added generic Nova test

---
+++ tests/Feature/Nova/ResourceBaselineTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Nova;
+
+use App\Nova\Resources;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Resource;
+use Tests\TestCase;
+use Tests\Traits\RequiresNova;
+
+class ResourceBaselineTest extends TestCase
+{
+    use RequiresNova;
+
+    /**
+     * Tests listing, viewing create, viewing.
+     *
+     * @dataProvider resourceProvider
+     */
+    public function test_resource(string $resourceClass): void
+    {
+        $this->assertTrue(is_a($resourceClass, Resource::class, true), 'Requested resource is not a Nova resource');
+
+        $modelClass = $resourceClass::$model;
+        $this->assertTrue(is_a($modelClass, Model::class, true), 'Resource model is not a model');
+
+        /** @var Model $model */
+        $model = factory($modelClass)->create();
+        $this->assertInstanceOf($modelClass, $model, 'Model class is not the same as the class passed to the test');
+
+        $resource = new $resourceClass($model);
+        assert($resource instanceof \Laravel\Nova\Resource);
+
+        $uriKey = call_user_func([get_class($resource), 'uriKey']);
+        $routeKey = $model->getKey();
+
+        $this->actingAs($this->getSuperAdminUser());
+
+        $this->get("/nova-api/{$uriKey}/creation-fields")
+            ->assertOk();
+
+        $this->get("/nova-api/{$uriKey}/{$routeKey}/update-fields")
+            ->assertOk();
+    }
+
+    public function resourceProvider(): array
+    {
+        $models = [
+            Resources\Activity::class,
+            Resources\EmailList::class,
+            Resources\FileBundle::class,
+            Resources\FileBundle::class,
+            Resources\JoinSubmission::class,
+            Resources\JoinSubmission::class,
+            Resources\Page::class,
+            Resources\RedirectInstruction::class,
+            Resources\User::class,
+            Resources\Webcam::class,
+        ];
+
+        return Collection::make($models)
+            ->mapWithKeys(fn ($row) => [class_basename($row) => [$row]])
+            ->all();
+    }
+}
