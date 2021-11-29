9e8a0c95fce2b667c32e766789f6138bd2412109
Roelof Roos <github@roelof.io>
2021-11-29 19:41:17 +0100

    Bind test locally

--- tests/Feature/Http/Middleware/RequirePaidEnrollmentTest.php
+++ tests/Feature/Http/Middleware/RequirePaidEnrollmentTest.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Tests\Feature\Http\Middleware;
 
 use App\Facades\Enroll;
+use App\Http\Middleware\RequirePaidEnrollment;
 use App\Models\Activity;
 use App\Models\User;
+use Illuminate\Support\Facades\Route;
 use Tests\TestCase;
 
 class RequirePaidEnrollmentTest extends TestCase
@@ -16,30 +18,36 @@ class RequirePaidEnrollmentTest extends TestCase
      */
     public function test_example()
     {
+        Route::get('/test/middleware/{activity}', fn (Activity $activity) => 'OK')
+            ->middleware([
+                RequireActiveEnrollment::class,
+                RequirePaidEnrollment::class,
+            ]);
+
         $activity = factory(Activity::class)->create();
         $ticket = $activity->tickets()->create([
             'title' => 'Free test',
         ]);
 
-        $this->get(route('test.paid-enrollment-middleware', [$activity]))
+        $this->get("/test/middleware/{$activity->getRouteKey()}")
             ->assertRedirect();
 
         $user = factory(User::class)->create();
         $this->actingAs($user);
 
-        $this->get(route('test.paid-enrollment-middleware', [$activity]))
+        $this->get("/test/middleware/{$activity->getRouteKey()}")
             ->assertRedirect();
 
         $enrollment = Enroll::createEnrollment($activity, $ticket);
         $this->assertNotNull($enrollment);
 
-        $this->get(route('test.paid-enrollment-middleware', [$activity]))
+        $this->get("/test/middleware/{$activity->getRouteKey()}")
             ->assertRedirect();
 
         $enrollment->price = 50_00;
         $enrollment->save();
 
-        $this->get(route('test.paid-enrollment-middleware', [$activity]))
+        $this->get("/test/middleware/{$activity->getRouteKey()}")
             ->assertOk();
     }
 }
