c73ed682227fc63a9e0f58efeebc8c3d671e4aba
Roelof Roos <github@roelof.io>
2022-04-13 15:00:40 +0200

    Custom Menu

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Models;
+use App\Nova\Dashboards;
+use App\Nova\Resources;
 use App\Nova\Resources\Permission;
 use App\Nova\Resources\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
 use Vyuldashev\NovaPermission\NovaPermissionTool;
@@ -24,6 +32,9 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
 
         // Ensure timezone is Europe/Amsterdam
         Nova::userTimezone(static fn () => 'Europe/Amsterdam');
+
+        // Create menu
+        $this->createMenu();
     }
 
     /**
@@ -91,7 +102,68 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function dashboards()
     {
         return [
-            new \App\Nova\Dashboards\Main(),
+            new Dashboards\Main(),
         ];
+    }
+
+    /**
+     * Ensure the menu of Nova is a bit prettier, not just one enormous list of items.
+     */
+    private function createMenu(): void
+    {
+        Nova::mainMenu(function (Request $request) {
+            $user = $request->user();
+
+            yield MenuItem::externalLink('Naar website', URL::to('/'));
+
+            yield MenuSection::dashboard(Dashboards\Main::class)->icon('chart-bar');
+
+            if ($user?->can('viewAny', Models\Activity::class)) {
+                yield MenuSection::make(__('Activities'), [
+                    MenuItem::resource(Resources\Activity::class),
+                    MenuItem::resource(Resources\Enrollment::class),
+                ])->icon('calendar')->collapsable();
+            }
+
+            if ($user?->can('viewAny', Models\Page::class)) {
+                yield MenuSection::make(__('Content'), [
+                    MenuItem::resource(Resources\Page::class),
+                    MenuItem::resource(Resources\NewsItem::class),
+                    MenuItem::resource(Resources\RedirectInstruction::class),
+                ])->icon('collection')->collapsable();
+            }
+
+            if ($user?->can('viewAny', Models\User::class)) {
+                yield MenuSection::make('Bestuurszaken', [
+                    MenuItem::resource(Resources\User::class),
+                    MenuItem::resource(Resources\EmailList::class),
+                    MenuItem::resource(Resources\JoinSubmission::class),
+                    MenuItem::resource(Resources\Sponsor::class),
+                ])->icon('users')->collapsable();
+            }
+
+            if ($user?->can('viewAny', Models\FileBundle::class)) {
+                yield MenuSection::make('Documentensysteem', [
+                    MenuItem::resource(Resources\FileBundle::class),
+                    MenuItem::resource(Resources\FileCategory::class),
+                ])->icon('library')->collapsable();
+            }
+
+            if ($user?->can('viewAny', Models\Shop\Order::class)) {
+                yield MenuSection::make(__('Shop'), [
+                    MenuItem::resource(Resources\Shop\Order::class),
+                    MenuItem::resource(Resources\Shop\Category::class),
+                    MenuItem::resource(Resources\Shop\ProductVariant::class),
+                    MenuItem::resource(Resources\Shop\Product::class),
+                ])->icon('shopping-cart')->collapsable();
+            }
+
+            if ($user?->can('devops')) {
+                yield MenuItem::make(__('DevOps'), [
+                    MenuItem::externalLink('Telescope', URL::to(Config::get('telescope.path'))),
+                    MenuItem::externalLink('Horizon', URL::to(Config::get('horizon.path'))),
+                ])->icon('server');
+            }
+        });
     }
 }
