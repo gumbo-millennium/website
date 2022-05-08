0facb75ba62a5e9e5dde8cc495c5841a6753b1ad
Roelof Roos <github@roelof.io>
2022-05-08 19:04:02 +0200

    Added AlpineJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,12 @@
  */
 
 import initModules from './modules'
+import Alpine from 'alpinejs'
 
 // Start app
 document.addEventListener('DOMContentLoaded', () => {
   initModules()
 })
+
+// Load alpine
+Alpine.start()
