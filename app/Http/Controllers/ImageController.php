7e5730f534e6dc5538d95798da8ff77f70e57d21
Roelof Roos <github@roelof.io>
2022-05-11 03:38:36 +0200

    Better handle 404 messages from Glade

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Filesystem\FileNotFoundException as FilesystemFileNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
+use League\Flysystem\FileNotFoundException;
 use League\Glide\Responses\LaravelResponseFactory;
 use League\Glide\ServerFactory;
 use Symfony\Component\HttpFoundation\StreamedResponse;
@@ -15,9 +19,9 @@ class ImageController extends Controller
 {
     /**
      * Renders the given image with the given glide parameters.
-     * Always streams the response.
+     * Always streams the response, if found.
      */
-    public function render(Request $request, string $path): StreamedResponse
+    public function render(Request $request, string $path): StreamedResponse|HttpResponse
     {
         $server = ServerFactory::create([
             'response' => new LaravelResponseFactory($request),
@@ -34,6 +38,9 @@ class ImageController extends Controller
             'base_url' => 'img',
         ]);
 
-        return $server->getImageResponse($path, $request->all());
-    }
+        try {
+            return $server->getImageResponse($path, $request->all());
+        } catch (FileNotFoundException | FilesystemFileNotFoundException) {
+            return Response::noContent(HttpResponse::HTTP_NOT_FOUND);
+        }
 }
