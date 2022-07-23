4ccf001ddde699e0ae6d059bb3d74c4568057852
Roelof Roos <github@roelof.io>
2022-07-23 15:28:07 +0200

    Explicitly resolve paths to phpcs folders

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,19 +7,19 @@ use PhpCsFixer\Finder;
 
 $finder = Finder::create()
     ->in([
-        '.deploy',
-        'app',
-        'config',
-        'database',
-        'resources/lang',
-        'routes',
-        'tests',
+        __DIR__ . '/.deploy',
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/resources/lang',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
     ]);
 
 return (new Config())
     ->setFinder($finder)
     ->setRiskyAllowed(true)
-    ->setCacheFile('.php-cs-fixer.cache')
+    ->setCacheFile(__DIR__ . '/.php-cs-fixer.cache')
     ->setRules([
         '@PSR12' => true,
         '@PHP74Migration:risky' => true,
