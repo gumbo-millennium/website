de59e7b6a5802e18ce81c8693e8e3c5e7a8a0331
Roelof Roos <github@roelof.io>
2022-10-09 01:37:16 +0200

    Translation fixes

--- resources/views/account/tickets.blade.php
+++ resources/views/account/tickets.blade.php
@@ -8,10 +8,14 @@
       {{-- Add all events --}}
       @foreach ($activities as $activity)
         <x-cards.activity :activity="$activity">
+          <x-slot name="lead">
+            {{ $activity->enrollment->ticket->title }}
+          </x-slot>
+
           <x-slot name="footer">
             <div class="flex flex-col lg:flex-row w-full gap-4">
               <x-button href="{{ route('enroll.show', $activity) }}" class="w-full" :disabled="$activity->end_date < now()">
-                @if ($activity->is_stable)
+                @if ($activity->enrollment->is_stable)
                   Beheren
                 @else
                   Afronden
@@ -19,7 +23,7 @@
               </x-button>
               @if ($googleWalletUrls->has($activity->id))
                 <x-button href="{{ $googleWalletUrls->get($activity->id) }}" target="_blank" style="night" class="w-full">
-                  Add to Google Wallet
+                  Toevoegen aan Google Wallet
                 </x-button>
               @endif
             </div>
