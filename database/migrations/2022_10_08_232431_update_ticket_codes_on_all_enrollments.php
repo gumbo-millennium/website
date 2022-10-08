b0a898f558d08e33766c7cecb342bff4197fb6ee
Roelof Roos <github@roelof.io>
2022-10-08 23:30:13 +0200

    Update ticket_code to allow 64 chars, instead of 16

---
+++ database/migrations/2022_10_08_232431_update_ticket_codes_on_all_enrollments.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Facades\Enroll;
+use App\Models\Enrollment;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Date;
+
+class UpdateTicketCodesOnAllEnrollments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $query = Enrollment::query()
+            ->whereHas('activity', fn ($query) => $query->where('end_date', '>', Date::now()))
+            ->active();
+
+        /** @var Enrollment $enrollment */
+        foreach ($query->lazy(100) as $enrollment) {
+            Enroll::updateTicketCode($enrollment);
+        }
+    }
+}
