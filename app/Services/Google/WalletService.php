767805e9dcda07a8b010c7e912e7b9b511f98c7e
Roelof Roos <github@roelof.io>
2022-12-28 22:35:07 +0100

    Fixed Google Wallet API acting up

--- app/Services/Google/WalletService.php
+++ app/Services/Google/WalletService.php
@@ -9,8 +9,7 @@ use App\Services\Google\Traits\HandlesEventClasses;
 use App\Services\Google\Traits\HandlesEventObjects;
 use App\Services\Google\Traits\HandlesJwtUrls;
 use App\Services\Google\Traits\HandlesModels;
-use Google\Client as GoogleClient;
-use Google_Service_Walletobjects as Walletobjects;
+use Google_Service_Walletobjects as GoogleWalletService;
 use Google_Service_Walletobjects_Eventticketclass_Resource as EventTicketClassResource;
 use Google_Service_Walletobjects_Eventticketobject_Resource as EventTicketObjectResource;
 use Illuminate\Support\Facades\App;
@@ -39,22 +38,13 @@ final class WalletService
         return $this->isEnabled;
     }
 
-    /**
-     * Returns the Google Walletobjects API.
-     * @return Walletobjects
-     */
-    protected function getGoogleWalletClient(): GoogleClient
-    {
-        return App::make('google_wallet_api');
-    }
-
     protected function getEventTicketClassApi(): EventTicketClassResource
     {
-        return App::make('google_wallet_eventticketclass_api');
+        return App::get(GoogleWalletService::class)->eventticketclass;
     }
 
     protected function getEventTicketObjectApi(): EventTicketObjectResource
     {
-        return App::make('google_wallet_eventticketobjects_api');
+        return App::get(GoogleWalletService::class)->eventticketobject;
     }
 }
