51b9b68f82a4742ccd045c2c354112f596c0fc2f
Roelof Roos <github@roelof.io>
2023-01-08 02:10:07 +0100

    Replace Price field with preconfigured Nova Currency field

--- app/Nova/Fields/Price.php
+++ app/Nova/Fields/Price.php
@@ -4,93 +4,24 @@ declare(strict_types=1);
 
 namespace App\Nova\Fields;
 
-use InvalidArgumentException;
-use Laravel\Nova\Fields\Number;
-use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Fields\Currency;
 
 /**
  * A price field.
  */
-class Price extends Number
+class Price extends Currency
 {
     /**
-     * Format of the displayed value.
+     * The symbol used by the currency.
      *
-     * @var string
+     * @var null|string
      */
-    protected $displayFormat = '€ %s';
+    public $currency = 'EUR';
 
     /**
-     * Sets the string used to format the price, should be a printf-compatible format.
-     */
-    public function displayFormat(string $format): self
-    {
-        // Validate format
-        if (! preg_match('/%(?!%)/', $format)) {
-            throw new InvalidArgumentException("Format [${format}] does not seem to be a valid sprintf string");
-        }
-
-        $this->displayFormat = $format;
-
-        return $this;
-    }
-
-    /**
-     * Resolve the field's value for display.
+     * Whether the currency is using minor units.
      *
-     * @param null|string $attribute
-     * @return void
+     * @var bool
      */
-    public function resolveForDisplay($resource, $attribute = null)
-    {
-        // Get value via parent
-        parent::resolveForDisplay($resource, $attribute);
-
-        // Format value
-        if ($this->value !== null) {
-            $this->value = sprintf($this->displayFormat, number_format($this->value, 2, ',', '.'));
-
-            return;
-        }
-
-        // Return empty value
-        $this->value = '–';
-    }
-
-    /**
-     * Hydrate the given attribute on the model based on the incoming request.
-     *
-     * @param string $requestAttribute
-     * @param object $model
-     * @param string $attribute
-     */
-    protected function fillAttributeFromRequest(
-        NovaRequest $request,
-        $requestAttribute,
-        $model,
-        $attribute
-    ) {
-        // Update value if present
-        if (! $request->exists($requestAttribute)) {
-            return;
-        }
-
-        $value = $request[$requestAttribute];
-
-        $model->{$attribute} = $this->isNullValue($value) ? null : (float) $value * 100;
-    }
-
-    /**
-     * Resolve the given attribute from the given resource.
-     *
-     * @param string $attribute
-     */
-    protected function resolveAttribute($resource, $attribute)
-    {
-        // Get value from parent
-        $value = parent::resolveAttribute($resource, $attribute);
-
-        // Divide by 100
-        return $value !== null ? $value / 100 : null;
-    }
+    public $minorUnits = true;
 }
