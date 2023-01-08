e303815a95f5a2bc39be3663ec58e355d5c888b9
Roelof Roos <github@roelof.io>
2023-01-08 07:00:50 +0100

    Ensure Google Wallet commands really don't run when disabled

--- app/Console/Commands/GoogleWallet/GoogleWalletCommand.php
+++ app/Console/Commands/GoogleWallet/GoogleWalletCommand.php
@@ -7,11 +7,25 @@ namespace App\Console\Commands\GoogleWallet;
 use App\Services\Google\WalletService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class GoogleWalletCommand extends Command
 {
     public function isHidden(): bool
     {
         return parent::isHidden() || ! App::make(WalletService::class)->isEnabled();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (! Config::get('services.google.wallet.enabled')) {
+            $this->error('Google Wallet is not enabled');
+
+            return 0;
+        }
+
+        return parent::execute($input, $output);
     }
 }
