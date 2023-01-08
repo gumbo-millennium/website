a45c4c9220be076dcc1dd8e0e38b9cbae0698f3c
Roelof Roos <github@roelof.io>
2023-01-08 01:06:49 +0100

    fixup! Change optimize command handling

--- app/Listeners/Console/CommandExtensionsListener.php
+++ app/Listeners/Console/CommandExtensionsListener.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace App\Listeners\Console;
 
+use Illuminate\Console\Command;
 use Illuminate\Console\Events\CommandStarting;
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Listen on the optimize commands and run additonal commands.
  */
 class CommandExtensionsListener
 {
+    private OutputInterface $output;
+
     /**
      * Handle the event.
      *
@@ -20,15 +24,20 @@ class CommandExtensionsListener
      */
     public function handle(CommandStarting $event)
     {
-        $call = fn (string $method, array $args = []) => Artisan::call($method, $args, $event->output);
+        $this->output = $event->output;
 
         if ($event->command == 'optimize') {
             // Create versioned pages
-            $call('gumbo:update-content');
+            $this->call('gumbo:update-content');
         }
 
         if ($event->command == 'optimize:clear') {
             //
         }
+    }
+
+    private function call(string $commandName, array $args = []): bool
+    {
+        return Artisan::call($commandName, $args, $this->output) === Command::SUCCESS;
     }
 }
