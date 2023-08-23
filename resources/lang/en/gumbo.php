18ff225294e758766f2a5c2186fb5dc081cc7eda
Roelof <github@roelofr.dev>
2023-08-23 18:13:26 +0200

    Added missing English translations (#340)

--- resources/lang/en/gumbo.php
+++ resources/lang/en/gumbo.php
@@ -12,4 +12,13 @@ return [
         PaymentStatus::CANCELLED => 'Cancelled',
         PaymentStatus::EXPIRED => 'Expired',
     ],
+
+    'bot-quotes' => [
+        'titles' => [
+            'week' => 'The quotes of week :week1 :year1',
+            'month' => 'The quotes of :month1 :year1',
+            'adjacent-months' => 'The quotes of :month1 :year1 and :month2 :year2',
+            'spanning-months' => 'The quotes of :month1 :year1 - :month2 :year2',
+        ],
+    ],
 ];
