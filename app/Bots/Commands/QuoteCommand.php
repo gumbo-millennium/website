5ef81c5fcbd799f25f63337de3f6c43ed7e8f70e
Roelof <github@roelofr.dev>
2023-10-22 13:06:33 +0200

    Small edit to trick the pipelines

--- app/Bots/Commands/QuoteCommand.php
+++ app/Bots/Commands/QuoteCommand.php
@@ -123,7 +123,7 @@ class QuoteCommand extends Command
         $user = $this->getUser();
 
         if (Str::length($quoteText) > self::MAX_QUOTE_LENGTH) {
-            Log::warn('Sender {user} sent overly long quote of {length} characters. Rejected.', [
+            Log::warn('Sender {user} sent overly long quote of {length} characters {quote}', [
                 'user' => $user ?? $tgUser,
                 'length' => Str::length($quoteText),
                 'quote' => Str::limit($quoteText, 500),
