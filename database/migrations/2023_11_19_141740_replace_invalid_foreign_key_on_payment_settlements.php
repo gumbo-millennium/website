0bfa86028fd7d873b98a83d685ab1f540ae1f264
Roelof Roos <github@roelofr.dev>
2023-11-19 16:04:18 +0100

    Fixed SQLite DB

--- database/migrations/2023_11_19_141740_replace_invalid_foreign_key_on_payment_settlements.php
+++ database/migrations/2023_11_19_141740_replace_invalid_foreign_key_on_payment_settlements.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class ReplaceInvalidForeignKeyOnPaymentSettlements extends Migration
@@ -15,6 +16,11 @@ class ReplaceInvalidForeignKeyOnPaymentSettlements extends Migration
      */
     public function up()
     {
+        // SQLite does not support dropping foreign keys, the schema was already changed.
+        if (DB::getDriverName() === 'sqlite') {
+            return;
+        }
+
         Schema::table('payment_settlement_payment', function (Blueprint $table) {
             $table->dropForeign('payment_settlement_payment_payment_id_foreign');
             $table->foreign('payment_id')->references('id')->on('payments')->onDelete('cascade');
