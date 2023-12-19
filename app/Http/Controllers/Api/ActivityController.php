7bf96b1b1e2a929932354a3080c564cb2706084c
Roelof Roos <github@roelofr.dev>
2023-12-19 16:47:29 +0100

    Added stubby register method

--- app/Http/Controllers/Api/ActivityController.php
+++ app/Http/Controllers/Api/ActivityController.php
@@ -45,6 +45,9 @@ class ActivityController extends Controller
         return ActivityResource::collection($activities)->toResponse($request);
     }
 
+    /**
+     * Display the given activity and the corresponding ticket(s).
+     */
     public function show(Request $request, string $slug): JsonResponse
     {
         $user = $request->user();
@@ -58,5 +61,40 @@ class ActivityController extends Controller
         abort_unless($activity, HttpResponse::HTTP_NOT_FOUND);
 
         return ActivityResource::make($activity)->toResponse($request);
+    }
+
+    /**
+     * Tries to register the user for the activity with the given slug, using the ticket specified
+     * in the body.
+     */
+    public function register(ActivityRegisterRequest $request, string $slug): JsonResponse
+    {
+        $user = $request->user();
+
+        /** @var Activity */
+        $activity = Activity::query()
+            ->whereAvailable($user)
+            ->wherePublished()
+            ->with('tickets', fn ($query) => $query->where('id', $request->ticket))
+            ->firstWhere('slug', $slug);
+
+        abort_unless($activity, HttpResponse::HTTP_NOT_FOUND);
+
+        /** @var Ticket */
+        $chosenTicket = $activity->tickets->first();
+        abort_unless($chosenTicket, HttpResponse::HTTP_NOT_FOUND);
+        abort_unless($chosenTicket->isAvailableFor($user), HttpResponse::HTTP_BAD_REQUEST);
+
+        if (! Enroll::canEnroll($activity)) {
+            abort(HttpResponse::HTTP_BAD_REQUEST, 'You cannot enroll in this activity.');
+        }
+
+        try {
+            $enrollment = Enroll::createEnrollment($activity, $chosenTicket);
+        } catch (Throwable) {
+            //
+        }
+
+        return Response::json(ActivityResource::make($activity));
     }
 }
