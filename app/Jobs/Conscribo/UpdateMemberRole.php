1195c4f86e1058bc855107c0d58b851c962d63d2
Roelof Roos <github@roelofr.dev>
2023-12-29 11:02:33 +0100

    Initial conscribo jobs

---
+++ app/Jobs/Conscribo/UpdateMemberRole.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs\Conscribo;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use RuntimeException;
+
+class UpdateMemberRole implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $this->fail(new RuntimeException('Not implemented yet'));
+    }
+}
