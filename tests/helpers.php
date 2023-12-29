a687f6ff242843b5365954f74523881eb21df823
Roelof Roos <github@roelofr.dev>
2023-12-29 11:01:26 +0100

    Added test_resource testing helper

--- tests/helpers.php
+++ tests/helpers.php
@@ -3,10 +3,27 @@
 declare(strict_types=1);
 
 if (! function_exists('test_path')) {
-    function test_path(string $file): ?string
+    /**
+     * Returns absolute path to a file in the tests folder.
+     */
+    function test_path(string $file): string
     {
         $file = ltrim($file, '/');
 
         return base_path("tests/{$file}");
+    }
+}
+
+if (! function_exists('test_resource')) {
+    /**
+     * Returns absolute path to a test resource, or the contents of it.
+     */
+    function test_resource(string $file, bool $returnContents = false): string
+    {
+        $file = ltrim($file, '/');
+
+        $path = test_path("Fixtures/resources/{$file}");
+
+        return $returnContents ? file_get_contents($path) : $path;
     }
 }
