686adb4ce76c05156149ef377431cbd8dff212d9
Roelof Roos <github@roelofr.dev>
2024-02-21 15:26:16 +0100

    WIP

--- app/Excel/Exports/Payments/SettlementExport.php
+++ app/Excel/Exports/Payments/SettlementExport.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Excel\Exports\Payments;
 
+use App\Models\Payment;
 use App\Models\Payments\Settlement;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\Exportable;
@@ -15,15 +16,36 @@ abstract class SettlementExport
     use Exportable;
 
     /**
+     * Payment models for reference.
+     * @var Collection<Payment>
+     */
+    public readonly Collection $paymentModels;
+
+    /**
      * @param Settlement $settlement Settlement to process
-     * @param Collection<MolliePayment> $payments Mollie payments for reference
-     * @param Collection<MollieRefund> $refunds Mollie refunds for reference
+     * @param Collection|MolliePayment[] $payments Mollie payments for reference
+     * @param Collection|MollieRefund[] $refunds Mollie refunds for reference
      */
     public function __construct(
         public readonly Settlement $settlement,
         public readonly Collection $payments,
         public readonly Collection $refunds,
     ) {
-        // no-op
+        $allPossibleIds = Collection::make()
+            ->push($payments->pluck('id'))
+            ->push($refunds->pluck('paymentId'))
+            ->unique()
+            ->values();
+
+        $this->paymentModels = Payment::query()
+            ->where(
+                fn ($query) => $query
+                    ->where('provider', 'mollie')
+                    ->whereIn('transaction_id', $allPossibleIds),
+            )
+
+            ->with('payable')
+            ->get()
+            ->keyBy('transaction_id');
     }
 }
--- app/Excel/Exports/Payments/SettlementExportSummarySheet.php
+++ app/Excel/Exports/Payments/SettlementExportSummarySheet.php
@@ -4,12 +4,43 @@ declare(strict_types=1);
 
 namespace App\Excel\Exports\Payments;
 
+use App\Models\Enrollment;
+use Illuminate\Support\Collection;
+use App\Models\Payment;
 use Maatwebsite\Excel\Concerns\FromArray;
 
 class SettlementExportSummarySheet extends SettlementExport implements FromArray
 {
     public function array(): array
     {
+        $rows = Collection::make();
+
+        $models = $this->paymentModels;
+
+        foreach ($this->payments as $payment) {
+            /** @var Payment $model */
+            $model = $models->get($payment->id) ?? $models->get($payment->orderId);
+
+            if (! $model || ! $model->payable instanceof Enrollment) continue;
+
+            $enrollment = $model->payable;
+            $activity = $enrollment->activity;
+
+            $activityRow = $rows->get($activity->id, [
+                'id' => $activity->id,
+                'name' => $activity->name,
+                'group' => $activity->group?->name ?? 'n/a',
+                'gross' => money_value(0),
+                'trans' => money_value(0),
+                'mollie' => money_value(0),
+                'buffer' => money_value(0),
+            ]);
+
+            $activityRow['gross'] = $activityRow['gross']->add(money_value($enrollment->total_price));
+            $activityRow['fees'] = $activityRow['fees']->add(money_value($enrollment->total_price - $enrollment->price));
+            $activityRow['mollie'] = $activityRow['mollie']->add(money_value($payment->))
+        }
+
         return [
             ['', 'Inkomstenbron', 'Bruto inkomsten', 'Totaal transactiekosten', 'Kosten Mollie', 'Transactiekosten buffer'],
         ];
