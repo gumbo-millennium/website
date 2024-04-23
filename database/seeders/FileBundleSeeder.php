f2ee8f4dd7e06b22002e633574891f9725822644
Roelof Roos <github@roelofr.dev>
2024-04-23 10:51:22 +0200

    Finally fix local seeders

--- database/seeders/FileBundleSeeder.php
+++ database/seeders/FileBundleSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use App\Models\FileBundle;
 use App\Models\FileCategory;
+use Faker\Generator;
 use Illuminate\Database\Seeder;
 
 class FileBundleSeeder extends Seeder
@@ -14,18 +15,17 @@ class FileBundleSeeder extends Seeder
      * Run the database seeds.
      *
      * @return void
-     * @throws Exception
      */
-    public function run()
+    public function run(Generator $faker)
     {
         // Create a bunch of categories
         /** @var array<FileCategory> $files */
-        $categories = FileCategory::factory()->times(random_int(1, 5))->create();
+        $categories = FileCategory::factory($faker->numberBetween(1, 5))->create();
 
         // Create a bunch of files for each category
         foreach ($categories as $category) {
             // Create a bunch of files in this category
-            FileBundle::factory()->times(random_int(2, 12))->create([
+            FileBundle::factory($faker->numberBetween(2, 12))->create([
                 'category_id' => $category->id,
             ]);
         }
