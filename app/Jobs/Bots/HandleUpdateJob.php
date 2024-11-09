7631d9c2d296655786efdc14c7b2f5867e0159a6
Roelof Roos <github@roelofr.dev>
2024-11-09 21:56:57 +0100

    Move chat storing to shared location

--- app/Jobs/Bots/HandleUpdateJob.php
+++ app/Jobs/Bots/HandleUpdateJob.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Jobs\Bots;
 
+use App\Models\Telegram\Chat;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Str;
 use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\Objects\Chat as TelegramChat;
 use Telegram\Bot\Objects\Update;
 
 class HandleUpdateJob
@@ -27,6 +30,9 @@ class HandleUpdateJob
      */
     public function handle(): void
     {
+        // Handle save
+        $this->saveChatData();
+
         // Handle reaction
         if ($this->update->isType('message_reaction')) {
             HandleUpdatedReactionJob::dispatchSync($this->update);
@@ -36,5 +42,25 @@ class HandleUpdateJob
         if ($this->update->isType('message')) {
             Telegram::bot()->processCommand($this->update);
         }
+    }
+
+    private function saveChatData(): void
+    {
+        $chat = object_get($this->update, 'message.chat');
+        if (!$chat instanceof TelegramChat)
+            return;
+
+        // Check variables
+        $chatId = (string)$chat->id;
+        $chatType = $chat->type;
+        $chatName = $chatType == 'private'
+            ? Str::trim("{$chat->firstName} {$chat->lastName}")
+            : $chat->title;
+
+        // Save the data
+        Chat::forChat($chatId)->fill([
+            'type' => $chatType,
+            'name' => Str::limit($chatName, 120),
+        ])->save();
     }
 }
