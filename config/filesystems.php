bbeaf743304769f5932e3b13b474090d8fac1837
Roelof Roos <github@roelofr.dev>
2024-12-05 08:57:05 +0100

    Support local temp urls;

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,6 +46,8 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
+            'serve' => true,
+            'throw' => false,
         ],
 
         'public' => [
